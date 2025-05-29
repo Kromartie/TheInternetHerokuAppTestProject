028603d4f3facecbe25eaf7596100d305dd5985f
Kevin Murphy <kmurphyk@hotmail.com>
2025-05-29 23:23:55 +0100

    removed old files

--- TheInternetHerokuApp/Configs/AppConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace TheInternetHerokuApp.Configs;
-public class TestSettings
-{
-    public string BaseUrl { get; set; }
-    public string Browser { get; set; }
-    public bool Headless { get; set; }
-}
